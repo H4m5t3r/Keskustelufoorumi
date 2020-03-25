4dcf20eecd174213dde5407900fe8731cfa0bc19
TheHamster <anton.taleiko@gmail.com>
2020-03-25 18:30:56 +0200

    new feature: create an account

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, SignUpForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -26,5 +26,24 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/new/")
+def auth_form():
+    return render_template("auth/new.html", form = SignUpForm())
+
+@app.route("/auth/", methods=["POST"])
+def auth_create():
+    form = SignUpForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/new.html", form = form)
+
+    t = User(form.name.data, form.username.data, form.password.data)
+  
+    db.session().add(t)
+    db.session().commit()
+  
     return redirect(url_for("index"))
 

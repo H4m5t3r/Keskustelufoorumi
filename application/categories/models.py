946f52f44d7784607fbfc8be394ae0c692e8a47d
TheHamster <anton.taleiko@gmail.com>
2020-04-11 16:43:19 +0300

    Message names no longer need to be unique.

--- application/categories/models.py
+++ application/categories/models.py
@@ -1,8 +1,14 @@
 from application import db
-from application.models import Base
 
-class Category(Base):
 
+class Category(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    name = db.Column(db.String(144), nullable=False)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False)
 

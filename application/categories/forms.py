7acd69efbcf8673647de7f09cb4fa34e1ff77e38
TheHamster <anton.taleiko@gmail.com>
2020-04-11 16:30:49 +0300

    Categories are now validated and are also unique.

--- application/categories/forms.py
+++ application/categories/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, validators
+from wtforms import BooleanField, StringField, validators, ValidationError
+from application.categories.models import Category
 
 class CategoryForm(FlaskForm):
     name = StringField("The new category", [validators.Length(min=1, max=20)])
  
+    def validate_name(self, name):
+        category = Category.query.filter_by(name=name.data).first()
+
+        if category:
+            raise ValidationError('That category already exists.')
+
     class Meta:
         csrf = False
